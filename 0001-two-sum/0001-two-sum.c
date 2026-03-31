9c420cef687bba998fda56394fee0eab82174c2b
Yann FERNANDEZ PUIG <63049466+yannfp@users.noreply.github.com>
2026-03-31 14:40:13 +0200

    0001-TWO-SUM Stats: Time: 55 ms (87.87%), Space: 8.6 MB (96.45%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.c
@@ -0,0 +1,34 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    int* result = malloc(sizeof(int) * 2);
+    if (!result)
+        return NULL;
+
+    for (int i = 0; i < numsSize; i++)
+    {
+        int n_1 = nums[i];
+
+        for (int j = i + 1; j < numsSize; j++)
+        {
+            int n_2 = nums[j];
+
+            if (n_1 + n_2 == target)
+            {
+                result[0] = i;
+                result[1] = j;
+
+                *returnSize = 2;
+
+                return result;
+            }
+        }
+    }
+
+    *returnSize = 0;
+    
+    free(result);
+
+    return NULL;
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # LeetCodeProblems
 A collection of LeetCode questions to ace the coding interview! - Created using [LeetHub 2.0](https://github.com/maitreya2954/LeetHub-2.0-Firefox)
+
+<!---LeetCode Topics Start-->
+# LeetCode Topics
+## Array
+|  |
+| ------- |
+| [0001-two-sum](https://github.com/yannfp/LeetCodeProblems/tree/master/0001-two-sum) |
+## Hash Table
+|  |
+| ------- |
+| [0001-two-sum](https://github.com/yannfp/LeetCodeProblems/tree/master/0001-two-sum) |
+<!---LeetCode Topics End-->
